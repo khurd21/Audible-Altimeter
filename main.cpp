95ec30a02a6d90ad5a5b1c2152a0ee9801746a77
Silas <63477128+SilasStokes@users.noreply.github.com>
2024-09-19 20:24:10 -0700

    added binary declarations to main, removed commented out code

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "hardware/dma.h"
+#include "pico/binary_info.h"
 
 #include <stdio.h>
 
@@ -6,21 +7,29 @@
 #include "audio_arrays/audio_samples.h"
 #include "pio_serializer.pio.h"
 
-// PIO sends one bit per 10 system clock cycles. DMA sends the same 32-bit
-// value 10 000 times before halting. This means we cycle through the 32 PWM
-// levels roughly once per second.
-#define PIO_SERIAL_CLKDIV 10.f
-#define PWM_REPEAT_COUNT 10000
-#define N_PWM_LEVELS 32
+// these are to convince the lsp that this symbol exists
+// but then they complain abou how this will static assert false erggg
+#ifndef PICO_AUDIO_I2S_CLOCK_PIN_BASE
+#define PICO_AUDIO_I2S_CLOCK_PIN_BASE 1
+// assert(false);
+#endif
+#ifndef PICO_AUDIO_I2S_DATA_PIN
+#define PICO_AUDIO_I2S_DATA_PIN 1
+// assert(false);
+#endif
+
+// these pin numbers are set in the cmake
+bi_decl(bi_1pin_with_name(PICO_AUDIO_I2S_CLOCK_PIN_BASE,
+                          "GPIO Pin # to 98357A BCLK"));
+bi_decl(bi_1pin_with_name(PICO_AUDIO_I2S_CLOCK_PIN_BASE + 1,
+                          "GPIO Pin # to 98357A LRC"));
+bi_decl(bi_1pin_with_name(PICO_AUDIO_I2S_DATA_PIN, "GPIO Pin # to 98357A DIN"));
 
 #define DMA_FINISHED_ISR_PIN 16
 
 int dma_chan;
 
 void dma_handler() {
-    static int pwm_level = 0;
-    static uint32_t wavetable[N_PWM_LEVELS];
-    static bool first_run = true;
     static bool even_odd{true};
     static uint counter { 0 };
 
@@ -32,11 +41,6 @@ void dma_handler() {
 
     dma_channel_set_trans_count(dma_chan, sample_lookup[counter].size, false);
 
-    // dma_channel_set_trans_count(
-    //     dma_chan, audio_test_48k_16bit_mono.size(), false);
-    // dma_channel_set_read_addr(dma_chan,
-    //                           audio_test_48k_16bit_mono.data(), true);
-
     dma_channel_set_read_addr(dma_chan,
                               sample_lookup[counter].location, true);
 
@@ -46,8 +50,6 @@ void dma_handler() {
         counter = 0;
     }
 }
-
-// unsigned int data_array[] = { 1, 2, 3, 4, 5, 6, 7, 8};
 
 int main() {
     gpio_init(DMA_FINISHED_ISR_PIN);
@@ -63,16 +65,12 @@ int main() {
     uint offset = pio_add_program(pio0, &pio_serialiser_program);
 
     uint pio_state_machine{0};
-    pio_serialiser_program_init(pio0, pio_state_machine, offset, PICO_AUDIO_I2S_DATA_PIN, PICO_AUDIO_I2S_CLOCK_PIN_BASE,
-                                PIO_SERIAL_CLKDIV);
+    pio_serialiser_program_init(pio0, pio_state_machine, offset, PICO_AUDIO_I2S_DATA_PIN, PICO_AUDIO_I2S_CLOCK_PIN_BASE);
 
-    // Configure a channel to write the same word (32 bits) repeatedly to PIO0
-    // SM0's TX FIFO, paced by the data request signal from that peripheral.
     dma_chan = dma_claim_unused_channel(true);
     dma_channel_config c = dma_channel_get_default_config(dma_chan);
     channel_config_set_transfer_data_size(&c, DMA_SIZE_16);
-    // channel_config_set_transfer_data_size(&c, DMA_SIZE_32);
-    // channel_config_set_read_increment(&c, false);
+
     channel_config_set_read_increment(&c, true);
     channel_config_set_write_increment(&c, false);
     channel_config_set_dreq(&c, DREQ_PIO0_TX0);
@@ -82,11 +80,8 @@ int main() {
         &pio0_hw->txf[0],  // Write address (only need to set this once)
         audio_test_48k_16bit_mono
             .data(),  // Don't provide a read address yet
-        // PWM_REPEAT_COUNT, // Write the same value many times, then halt and
-        // interrupt
         audio_test_48k_16bit_mono.size(),
-        // 100,
-        false  // Don't start yet
+        false // don't start transfer
     );
 
     // Tell the DMA to raise IRQ line 0 when the channel finishes a block
@@ -98,55 +93,7 @@ int main() {
 
     dma_channel_start(dma_chan);
 
-    // Manually call the handler once, to trigger the first transfer
-    // dma_handler();
-
-    // Everything else from this point is interrupt-driven. The processor has
-    // time to sit and think about its early retirement -- maybe open a bakery?
     while (true) {
         tight_loop_contents();
     }
 }
-
-// #include <stdio.h>
-// #include "pico/stdlib.h"
-// #include "hardware/dma.h"
-
-// // Data will be copied from src to dst
-// const char src[] = "Hello, world! (from DMA)";
-// char dst[count_of(src)];
-
-// int main() {
-//     stdio_init_all();
-
-//     // Get a free channel, panic() if there are none
-//     int chan = dma_claim_unused_channel(true);
-
-//     // 8 bit transfers. Both read and write address increment after each
-//     // transfer (each pointing to a location in src or dst respectively).
-//     // No DREQ is selected, so the DMA transfers as fast as it can.
-
-//     dma_channel_config c = dma_channel_get_default_config(chan);
-//     channel_config_set_transfer_data_size(&c, DMA_SIZE_8);
-//     channel_config_set_read_increment(&c, true);
-//     channel_config_set_write_increment(&c, true);
-
-//     dma_channel_configure(
-//         chan,          // Channel to be configured
-//         &c,            // The configuration we just created
-//         dst,           // The initial write address
-//         src,           // The initial read address
-//         count_of(src), // Number of transfers; in this case each is 1 byte.
-//         true           // Start immediately.
-//     );
-
-//     // We could choose to go and do something else whilst the DMA is doing
-//     its
-//     // thing. In this case the processor has nothing else to do, so we just
-//     // wait for the DMA to finish.
-//     dma_channel_wait_for_finish_blocking(chan);
-
-//     // The DMA has now copied our text from the transmit buffer (src) to the
-//     // receive buffer (dst), so we can print it out from there.
-//     puts(dst);
-// }
