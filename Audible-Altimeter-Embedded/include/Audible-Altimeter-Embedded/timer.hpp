77c921e0dcb2d0801c3d7d76086db117e8828001
Kyle Hurd <kyle.hurd@wsu.edu>
2024-10-06 23:58:29 -0400

    7: Begins logic for documenting a jump

---
+++ Audible-Altimeter-Embedded/include/Audible-Altimeter-Embedded/timer.hpp
@@ -0,0 +1,24 @@
+#ifndef AUDIBLE_ALTIMETER_EMBEDDED_TIMER_HPP
+#define AUDIBLE_ALTIMETER_EMBEDDED_TIMER_HPP
+
+#include <hardware/i2c.h>
+#include <hardware/timer.h>
+
+#include <Audible-Altimeter/timer_interface.hpp>
+
+namespace altimeter {
+
+class Timer : public ITimer {
+ public:
+  void add_repeating_timer(std::chrono::milliseconds interval,
+                           CallbackData& callback_data) override;
+
+ private:
+  static bool timer_callback(repeating_timer_t* rt);
+
+  repeating_timer_t m_timer;
+};
+
+}  // namespace altimeter
+
+#endif  // AUDIBLE_ALTIMETER_EMBEDDED_TIMER_HPP
--- Audible-Altimeter/src/runner.cpp
+++ Audible-Altimeter/src/runner.cpp
@@ -1,24 +1,40 @@
 #include <Audible-Altimeter/barometric_sensor_interface.hpp>
 #include <Audible-Altimeter/device_description_interface.hpp>
 #include <Audible-Altimeter/runner.hpp>
+#include <Audible-Altimeter/timer_interface.hpp>
 #include <cstdio>
 
 namespace altimeter {
 
-Runner::Runner(IDeviceDescription* tiny, IBarometricSensor* barometric_sensor) {
+Runner::Runner(IDeviceDescription* tiny, IBarometricSensor* barometric_sensor,
+               ITimer* timer) {
   m_tiny_2350 = tiny;
   m_barometric_sensor = barometric_sensor;
+  m_timer = timer;
+  m_callback_data.callback = &Runner::read_event_callback;
+  m_callback_data.user_data = this;
 }
 
 void Runner::run() {
   m_tiny_2350->toggle_leds(false);
-  while (true) {
-    const auto data{m_barometric_sensor->get_sensor_data()};
-    if (data) {
-      printf("Data T: %.2f deg C, P: %.2f Pa\n", data->temperature,
-             data->pressure);
-    }
+  m_timer->add_repeating_timer(std::chrono::milliseconds(500), m_callback_data);
+};
+
+bool Runner::read_event() {
+  const auto data{m_barometric_sensor->get_sensor_data()};
+  if (data) {
+    printf("Data T: %.2f deg C, P: %.2f Pa\n", data->temperature,
+           data->pressure);
   }
+  return true;
+}
+
+bool Runner::read_event_callback(void* user_data) {
+  auto data{static_cast<Runner*>(user_data)};
+  if (data) {
+    return data->read_event();
+  }
+  return false;
 }
 
 }  // namespace altimeter
