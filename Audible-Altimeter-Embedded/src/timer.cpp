36b5229aec65a3bcd261f58bab8af4fe4cd1c839
Kyle Hurd <kyle.hurd@wsu.edu>
2024-10-31 01:56:51 -0400

    18: Fixes timer crash

--- Audible-Altimeter-Embedded/src/timer.cpp
+++ Audible-Altimeter-Embedded/src/timer.cpp
@@ -9,9 +9,15 @@ void Timer::add_repeating_timer(std::chrono::milliseconds interval,
                                 CallbackData& callback_data) {
   add_repeating_timer_ms(interval.count(), &Timer::timer_callback,
                          &callback_data, &m_timer);
+  m_timer_added = true;
 }
 
-void Timer::remove_repeating_timer() { cancel_repeating_timer(&m_timer); }
+void Timer::remove_repeating_timer() {
+  if (m_timer_added) {
+    cancel_repeating_timer(&m_timer);
+    m_timer_added = false;
+  }
+}
 
 bool Timer::timer_callback(repeating_timer_t* rt) {
   auto data{static_cast<CallbackData*>(rt->user_data)};
